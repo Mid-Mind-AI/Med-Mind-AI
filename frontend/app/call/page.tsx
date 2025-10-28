a06b5603bb698e196f1301e186444aa13f62dc7a
Syed-Ahmed02 <syedahmedd.02@gmail.com>
2025-10-27 20:59:09 -0400

    created voice input screen added sidebar

---
+++ frontend/app/call/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { AIVoiceInput } from "@/components/ui/ai-voice-input";
+export default function Call() {
+  return (
+    <div className="  flex flex-col items-center justify-center bg-background p-4 gap-16 mt-16">
+            <h1 className="text-4xl font-bold max-w-lg md:max-w-xl text-center">Start a call to get started booking your appointment</h1>
+            <div className="flex justify-center">
+                <AIVoiceInput className="bg-secondary rounded-lg p-4 w-fit" />
+            </div>
+    </div>
+  );
+}
