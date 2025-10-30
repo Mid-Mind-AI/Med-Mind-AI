b20d68f7df4cdc823b6e886d920e98ff7cda9080
Syed <syedahmedd.02@gmail.com>
2025-10-30 12:06:28 -0400

    completed landing page

--- frontend/app/(user)/layout.tsx
+++ frontend/app/(user)/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
+import "@/app/globals.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,9 +27,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <SidebarProvider>
+          <AppSidebar />
           <main className="flex-1 flex flex-col min-h-screen bg-secondary">
             {children}
           </main>
+        </SidebarProvider>
       </body>
     </html>
   );
